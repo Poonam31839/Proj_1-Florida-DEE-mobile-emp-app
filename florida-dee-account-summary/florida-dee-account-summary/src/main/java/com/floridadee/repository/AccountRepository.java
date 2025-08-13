b6a872cba22954a32a00fed4682354cd02841a15
Poonam31839 <poonam6528864@gmail.com>
2025-08-13 16:53:11 +0530

    Update AccountRepository.java

--- florida-dee-account-summary/florida-dee-account-summary/src/main/java/com/floridadee/repository/AccountRepository.java
+++ florida-dee-account-summary/florida-dee-account-summary/src/main/java/com/floridadee/repository/AccountRepository.java
@@ -1,1 +1,11 @@
 // Placeholder for AccountRepository.java
+
+package com.floridadee.repository;
+
+import com.floridadee.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByActiveTrue();
+}
