5e8c3ba8c08c36b8b9bbcbe0b7f3e5b7c2ea3662
Poonam31839 <poonam6528864@gmail.com>
2025-08-13 16:53:56 +0530

    Update AccountSummaryService.java

--- florida-dee-account-summary/florida-dee-account-summary/src/main/java/com/floridadee/service/AccountSummaryService.java
+++ florida-dee-account-summary/florida-dee-account-summary/src/main/java/com/floridadee/service/AccountSummaryService.java
@@ -1,1 +1,18 @@
-// Placeholder for AccountSummaryService.java
+package com.floridadee.service;
+
+import com.floridadee.model.Account;
+import com.floridadee.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class AccountSummaryService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public List<Account> getAllActiveAccounts() {
+        return accountRepository.findByActiveTrue();
+    }
+}
