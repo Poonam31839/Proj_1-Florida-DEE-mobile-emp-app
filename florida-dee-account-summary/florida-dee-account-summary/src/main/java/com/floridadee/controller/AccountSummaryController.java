da4bfcb29c0c25d494d014fb0c15288f95bcacdf
Poonam31839 <poonam6528864@gmail.com>
2025-08-13 16:57:21 +0530

    Update AccountSummaryController.java

--- florida-dee-account-summary/florida-dee-account-summary/src/main/java/com/floridadee/controller/AccountSummaryController.java
+++ florida-dee-account-summary/florida-dee-account-summary/src/main/java/com/floridadee/controller/AccountSummaryController.java
@@ -1,1 +1,25 @@
-// Placeholder for AccountSummaryController.java
+package com.floridadee.controller;
+
+import com.floridadee.model.Account;
+import com.floridadee.service.AccountSummaryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountSummaryController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountSummaryController.class);
+
+    @Autowired
+    private AccountSummaryService accountService;
+
+    @GetMapping("/active")
+    public List<Account> getActiveAccounts() {
+        logger.info("Fetching all active accounts");
+        return accountService.getAllActiveAccounts();
+    }
+}
